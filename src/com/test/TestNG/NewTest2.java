94e3bf7a1328df424238c683398918039da06bd6
Prasanga Fernando <prasangafdz@outlook.com>
2020-09-15 18:33:50 +0530

    Added another class

--- src/com/test/TestNG/NewTest2.java
+++ src/com/test/TestNG/NewTest2.java
@@ -4,41 +4,39 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class NewTest {
-	//This will require basic knowledge in Threads
-	//Created by Prasanga Fernando on 2020.09.13
-	
-	@BeforeClass
-	public void initiateTest() {
-		Assert.assertEquals("test", "test");
-		System.out.println("Method name is InitialTest and thread id: "+Thread.currentThread().getId()+ " ");
-	}
-	@Test (priority =1, groups = {"smoke", "Performance"})
+public class NewTest2 {
+
+//	@BeforeClass
+//	public void initiateTest() {
+//		Assert.assertEquals("test", "test");
+//		System.out.println("Method name is InitialTest and thread id: "+Thread.currentThread().getId()+ " "+Thread.currentThread().getName());
+//	}
+	@Test (priority =7, groups = {"smoke", "Performance"})
 	public void openPage() {
 		Assert.assertEquals("test", "test");
 		System.out.println("Method name is openPage and thread id: "+Thread.currentThread().getId()+ " ");
 	}
-	@Test (priority =2, groups = {"smoke", "Performance"})
+	@Test (priority =8, groups = {"smoke", "Performance"})
 	public void login() {
 		Assert.assertEquals("test", "test");
 		System.out.println("Method name is login and thread id: "+Thread.currentThread().getId()+ " ");
 	}
-	@Test (priority =3, groups = {"smoke", "Performance"})
+	@Test (priority =9, groups = {"smoke", "Performance"})
 	public void addNewUser() {
 		Assert.assertEquals("test", "test");
 		System.out.println("Method name is addNewUser and thread id: "+Thread.currentThread().getId()+ " ");
 	}
-	@Test (priority =4, groups = {"Performance"})
+	@Test (priority =10, groups = {"Performance"})
 	public void viewUsers() {
 		Assert.assertEquals("test", "test");
 		System.out.println("Method name is viewUsers and thread id: "+Thread.currentThread().getId()+ " ");
 	}
-	@Test (priority =5, groups = {"Performance"})
+	@Test (priority =11, groups = {"Performance"})
 	public void removeExistingUser() {
 		Assert.assertEquals("test", "test");
 		System.out.println("Method name is removeExistingUser and thread id: "+Thread.currentThread().getId()+ " ");
 	}
-	@Test (priority =6, groups = {"smoke", "Performance"})
+	@Test (priority =12, groups = {"smoke", "Performance"})
 	public void Logout() {
 		Assert.assertEquals("test", "test");
 		System.out.println("Method name is Logout and thread id: "+Thread.currentThread().getId()+ " ");
